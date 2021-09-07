2caece27db26d855676130ba2dd1ba07b91b5a02
Rafael Ramirez Quiroz <89743714+raforam@users.noreply.github.com>
2021-09-06 23:47:55 -0500

    Create substitution.cpp

---
+++ substitution.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string text = 
+  "lrvmnir bpr sumvbwvr jx bpr lmiwv yjeryrkbi jx qmbm wi bpr xjvni mkd ymibrut jx irhx wi bpr riirkvr jx ymbinlmtmipw utn qmumbr dj w ipmhh but bj rhnvwdmbr bpr yjeryrkbi jx bpr qmbm mvvjudwko bj yt wkbrusurbmbwjk lmird jk xjubt trmui jx ibndt wb wi kjb mk rmit bmiq bj rashmwk rmvp yjeryrkb mkd wbi iwokwxwvmkvr mkd ijyr ynib urymwk nkrashmwkrd bj ower m vjyshrbr rashmkmbwjk jkr cjnhd pmer bj lr fnmhwxwrd mkd wkiswurd bj invp mk rabrkb bpmb pr vjnhd urmvp bpr ibmbr jx rkhwopbrkrd ywkd vmsmlhr jx urvjokwgwko ijnkdhrii ijnkd mkd ipmsrhrii ipmsr w dj kjb drry ytirhx bpr xwkmh mnbpjuwbt lnb yt rasruwrkvr cwbp qmbm pmi hrxb kj djnlb bpmb bpr xjhhjcwko wi bpr sujsru msshwvmbwjk mkd wkbrusurbmbwjk w jxxru yt bprjuwri wk bpr pjsr bpmb bpr riirkvr jx jqwkmcmk qmumbr cwhh urymwk wkbmvb";
+  
+  int a=0,b=0,c=0,d=0,e=0,f=0,g=0,h=0,i=0,j=0,k=0,l=0,m=0,n=0,o=0,p=0,q=0,r=0,s=0,t=0,u=0,v=0,w=0,x=0,y=0,z=0;
+
+  for(int i=0; i<=text.length(); i++){
+    if(text[i] == 'a'){
+      a+=1;
+    }
+    else if(text[i] == 'b'){
+      b+=1;
+    }
+    else if(text[i] == 'c'){
+      c+=1;
+    }
+    else if(text[i] == 'd'){
+      d+=1;
+    }
+    else if(text[i] == 'e'){
+      e+=1;
+    }
+    else if(text[i] == 'f'){
+      f+=1;
+    }
+    else if(text[i] == 'g'){
+      g+=1;
+    }
+    else if(text[i] == 'h'){
+      h+=1;
+    }
+    else if(text[i] == 'i'){
+      i+=1;
+    }
+    else if(text[i] == 'j'){
+      j+=1;
+    }
+    else if(text[i] == 'k'){
+      k+=1;
+    }
+    else if(text[i] == 'l'){
+      l++;
+    }
+    else if(text[i] == 'm'){
+      m+=1;
+    }
+    else if(text[i] == 'n'){
+      n+=1;
+    }
+    else if(text[i] == 'o'){
+      o+=1;
+    }
+    else if(text[i] == 'p'){
+      p+=1;
+    }
+    else if(text[i] == 'q'){
+      q+=1;
+    }
+    else if(text[i] == 'r'){
+      r+=1;
+    }
+    else if(text[i] == 's'){
+      s+=1;
+    }
+    else if(text[i] == 't'){
+      t+=1;
+    }
+    else if(text[i] == 'u'){
+      u+=1;
+    }
+    else if(text[i] == 'v'){
+      v+=1;
+    }
+    else if(text[i] == 'w'){
+      w+=1;
+    }
+    else if(text[i] == 'x'){
+      x+=1;
+    }
+    else if(text[i] == 'y'){
+      y+=1;
+    }
+    else if(text[i] == 'z'){
+      z+=1;
+    }
+  }
+
+  cout<<"a: "<<a<<endl;
+  cout<<"b: "<<b<<endl;
+  cout<<"c: "<<c<<endl;
+  cout<<"d: "<<d<<endl;
+  cout<<"e: "<<e<<endl;
+  cout<<"f: "<<f<<endl;
+  cout<<"g: "<<g<<endl;
+  cout<<"h: "<<h<<endl;
+  cout<<"i: "<<i<<endl;
+  cout<<"j: "<<j<<endl;
+  cout<<"k: "<<k<<endl;
+  cout<<"l: "<<l<<endl;
+  cout<<"m: "<<m<<endl;
+  cout<<"n: "<<n<<endl;
+  cout<<"o: "<<o<<endl;
+  cout<<"p: "<<p<<endl;
+  cout<<"q: "<<q<<endl;
+  cout<<"r: "<<r<<endl;
+  cout<<"s: "<<s<<endl;
+  cout<<"t: "<<t<<endl;
+  cout<<"u: "<<u<<endl;
+  cout<<"v: "<<v<<endl;
+  cout<<"w: "<<w<<endl;
+  cout<<"x: "<<x<<endl;
+  cout<<"y: "<<y<<endl;
+  cout<<"z: "<<z<<endl;
+
+
+
+  return 0;
+
+}
